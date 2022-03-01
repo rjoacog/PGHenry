e90997defa453303a833d0dbfeef7d20109dbb0b
Joaquin <rjoacog@gmail.com>
2022-03-01 17:25:22 -0300

    configuracion inicial

---
+++ backend/ProductTable.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts, setAddOrUpdate } from "../Redux/Actions/actions";
+
+const ProductTable = ({ setIsOpen }) => {
+  const { products, loadProducts } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  if(loadProducts) dispatch(getProducts());
+  
+  const handleClick = (product) => {
+    dispatch(setAddOrUpdate(product))
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <table>
+      <thead className="panel-table--head">
+        <tr>
+          <th className="panel-table--th">Producto</th>
+          <th className="panel-table--th">Precio</th>
+          <th className="panel-table--th">Cantidad</th>
+          <th className="panel-table--th">Opcion</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products?.map((product) => (
+          <tr key={product.id + product.name}>
+            <td className="panel-table--td">{product.name}</td>
+            <td className="panel-table--td">${product.price}</td>
+            <td className="panel-table--td">{product.quantity}</td>
+            <td className="panel-table--td">
+              <button className="panel-table--td_buttons" onClick={() => handleClick(product)}>Editar</button>
+            </td>
+          </tr>
+        ))}
+        {products?.length === 0 && (
+          <tr>
+            <td colSpan="4" className="panel-table--td opacity">
+              No hay productos
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default ProductTable;

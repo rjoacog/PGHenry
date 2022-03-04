39095857b439251b04610eb8ad9be97ef8c2fba6
Joaquin <rjoacog@gmail.com>
2022-03-04 11:54:33 -0300

    NavBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Home from './components/Home';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
    <BrowserRouter>
    <div className="App">
+     <NavBar />
    <Routes>
      {/* <Route exact path="/" element={<LandingPage/>} /> */}
      <Route exact path="/" element={<Home/>} />

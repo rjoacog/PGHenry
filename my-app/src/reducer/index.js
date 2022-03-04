95313a42813abcd9d6b68609c7a368f7cf1ddf85
Nacho Olivera <oliveraignaciook@gmail.com>
2022-03-03 21:24:14 -0300

    Update Home and Cards

--- my-app/src/reducer/index.js
+++ my-app/src/reducer/index.js
@@ -1,21 +1,23 @@
 import { GET_PRODUCTS } from "../actions/types";
 
 const initialState = {
-    allProducts=[], //estado constante q no se modifica 
-    products=[] // auxiliar que se ve modificado al necesitar un  filter o un sort
+  allProducts: [],
+  products: [],
+};
+
+function rootReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        allProducts: payload,
+        products: payload,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
 }
-
-
-function rootReducer(state = initialState, {type, payload}) {
-    switch(type){
-        case GET_PRODUCTS:
-            return{
-                ...state,
-                allProducts: payload,
-                products: payload
-            }
-    }
-}
-
 
 export default rootReducer;

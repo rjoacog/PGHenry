76bc4772015398c207b295319df1f423427ef33b
Joaquin <rjoacog@gmail.com>
2022-03-04 14:16:07 -0300

    Formulario Usuario

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Home from './components/Home';
 import NavBar from "./components/NavBar";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
    <Routes>
      {/* <Route exact path="/" element={<LandingPage/>} /> */}
      <Route exact path="/" element={<Home/>} />
+     <Route path="/register" element={<UserForm />} />
    </Routes>
     </div>
     </BrowserRouter>

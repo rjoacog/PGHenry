d72041e0a3fd9b9edd2fd8b7416f1ecdd54b4858
JuanmaGre <jmanuelgrehuello@yahoo.com>
2022-03-11 11:46:46 -0300

    modificaciones del shopping cart

--- backend/src/routes/shoppingCart.js
+++ backend/src/routes/shoppingCart.js
@@ -1,15 +1,20 @@
-const { Router } = require('express');
-const { addCartItem, getCartEmpty, getAllCartItems, editCartQuantity, deleteCartItem } = require('../controllers/shoppingCart')
+const express = require("express");
+const router = express.Router();
+const {
+  shoppingCart,
+  addItemCart,
+  updateAndRemoveItemCart,
+  removeItemCart,
+  clearCart
+} = require("../controllers/shoppingCart");
 
-const router = Router();
 
 
-
-router.get('/cart/:idUser', getAllCartItems);
-router.post('/cart/:idUser', addCartItem);
-router.delete('/cart/:idUser', getCartEmpty);
-router.put('/cart/:idUser', editCartQuantity, getAllCartItems);
-router.delete('/cart/:idUser/:idProduct', deleteCartItem);
+router.post("/", shoppingCart);
+router.post("/:id", addItemCart);
+router.put("/:id",  updateAndRemoveItemCart);
+router.put("/delete/:id", removeItemCart);
+router.delete("/", clearCart);
 
 
 

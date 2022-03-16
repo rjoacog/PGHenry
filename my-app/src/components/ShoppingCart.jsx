89ce89ebc0cbd04a3220914bac2757307cdeef80
Joaquin <rjoacog@gmail.com>
2022-03-16 19:00:41 -0300

    BotonPago

--- my-app/src/components/ShoppingCart.jsx
+++ my-app/src/components/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearAllCart, delOneCart, delAllCart } from '../actions/creates';
 import CartItem from './CartItem';
@@ -6,8 +6,30 @@ import { Button, Text, Box } from '@chakra-ui/react';
 
 export default function ShoppingCart() {
 
+    
+    const [totalItems, setTotalItems] = useState(0)
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [totalCart, setTotalCart] = useState(0)
+
     const cart = useSelector((state) => state.cart)
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        let itemCarts = 0;
+        let priceCart = 0;
+        let totalCart = 0;
+
+        cart.forEach(item => {
+            itemCarts += item.quantity
+            priceCart += item.price * item.quantity
+            totalCart += priceCart
+        })
+
+        setTotalItems(itemCarts)
+        setTotalPrice(priceCart)
+        setTotalCart(totalCart)
+        localStorage.setItem('items', JSON.stringify(cart));
+    }, [cart, totalItems, totalPrice, totalCart])
 
 
     const clearCart = () => {
@@ -40,7 +62,12 @@ export default function ShoppingCart() {
                   _id={p._id}
                   quantity= {p.quantity}
                 />)
-            }     
+            } 
+             <Box bg='gray.100' w='100%' p={4} color='black'>
+                    <Text fontSize='3xl'>TOTAL : ${totalCart} </Text>
+                    < br/>
+                    <Button colorScheme='blue'>PROCEED TO CHECKOUT</Button>
+                    </Box>    
         </div>
         </Box>
     )
